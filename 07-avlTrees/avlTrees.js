fc3da3567d2392a196483c19a08038489b257f1b
Kevin Hung <kevincjhung@gmail.com>
2024-07-13 15:27:39 -0700

    chore: Init AVL Tree directory

---
+++ 07-avlTrees/avlTrees.js
@@ -0,0 +1,44 @@
+class Node {
+	/**
+	 * Creates an instance of a Node.
+	 * @param {*} content - The content to be stored in the node.
+	 * @param {Node|null} parent - The parent node.
+	 * @param {Node|null} left - The left child node.
+	 * @param {Node|null} right - The right child node.
+	 */
+	constructor(content, parent = null, left = null, right = null) {
+		this.content = content; // Content of the node, can be any data type
+		this.parent = parent;   // Pointer to the parent node
+		this.left = left;       // Pointer to the left child node
+		this.right = right;     // Pointer to the right child node
+	}
+
+	/**
+	 * Sets the left child of the node.
+	 * @param {Node} node - The node to be set as the left child.
+	 */
+	setLeft(node) {
+		this.left = node;
+		if (node) {
+			node.parent = this;
+		}
+	}
+
+	/**
+	 * Sets the right child of the node.
+	 * @param {Node} node - The node to be set as the right child.
+	 */
+	setRight(node) {
+		this.right = node;
+		if (node) {
+			node.parent = this;
+		}
+	}
+}
+
+
+
+
+module.exports = {
+  Node
+};
